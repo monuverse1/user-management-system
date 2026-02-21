f8b2aae913a45dbfaba829aecb5779bdcf33bbd6
Monu Chaurasia <monut272175@gmail.com>
2026-02-21 09:20:23 +0530

    final fullstack setup completed

---
+++ backend/expres.js
@@ -0,0 +1,41 @@
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+// middleware to use json body
+app.use(express.json());
+mongoose
+   .connect(
+      "mongodb+srv://Monu:virat18@cluster2.ynd93mj.mongodb.net/?appName=Cluster2",{
+        
+      }
+   )
+   .then(()=>console.log("MongoDB connection"))
+   .catch((err => console.log("MonogoDB connection error:",err)))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+}) 
+
+app.get('/login', (req, res) => {
+  res.send('Please login first!')
+}) 
+
+app.get('/signup', (req, res) => {
+  res.send('Please signup first!')
+}) 
+
+app.get('/play', (req, res) => {
+  res.send('come and play!')
+}) 
+app.get('/cricket', (req, res) => {
+  res.send('Cricket is world wide game!')
+}) 
+app.get('/login/signup/play/cricket/virat', (req, res) => {
+  res.send('King Kohli!')
+}) 
+
+app.listen(4500, () => {
+  console.log(`Example app listening on port `)
+})           
